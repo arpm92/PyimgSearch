1e9c277b7e531cc2109b88f7783ca7c990efcd23
arpm92 <arpm92@gmail.com>
2021-07-26 22:38:19 +0200

    added hardcoded dict

--- command-line-arguments/simple_example.py
+++ command-line-arguments/simple_example.py
@@ -11,5 +11,7 @@ ap.add_argument("-n", "--name", required=True,
 	help="name of the user")
 args = vars(ap.parse_args())
 
+# args = {"name": "Alejandro"} # dictionary used instead of argparse
+
 # display a friendly message to the user
 print("Hi there {}, it's nice to meet you!".format(args["name"]))
